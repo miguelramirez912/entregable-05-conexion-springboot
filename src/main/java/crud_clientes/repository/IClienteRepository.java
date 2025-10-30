c53a26213715d6fa9317c1ac975e5a480ddb6cd0
Miguel Angel Ramirez Herrera <miguel.ramirezh@infotec.mx>
2025-10-30 14:20:35 -0600

    Implementado el ClienteService con sus metodos find, create y delete. Creado el ClienteDto para el metodo create del ClienteService

--- src/main/java/crud_clientes/repository/IClienteRepository.java
+++ src/main/java/crud_clientes/repository/IClienteRepository.java
@@ -1,5 +1,9 @@
 package crud_clientes.repository;
 
-public interface IClienteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import crud_clientes.entity.Cliente;
+
+public interface IClienteRepository extends JpaRepository <Cliente, Long> {
 
 }
---
+++ src/main/java/crud_clientes/service/ClienteService.java
@@ -0,0 +1,44 @@
+package crud_clientes.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import crud_clientes.dto.ClienteDto;
+import crud_clientes.entity.Cliente;
+import crud_clientes.repository.IClienteRepository;
+
+public class ClienteService {
+	
+	@Autowired
+	private IClienteRepository clienteRepository;
+	
+	//Consulta de todos los clientes
+	@Transactional(readOnly = true)
+	public List<Cliente> findAll(){
+		return (List<Cliente>)clienteRepository.findAll();
+	}
+	
+	//Consulta por Id
+	@Transactional(readOnly = true)
+	public Cliente findById(Long id) {
+		return (Cliente) clienteRepository.findById(id).orElse(null);
+	}
+	
+	//CrearCliente
+	@Transactional
+	public Cliente createCliente(ClienteDto clienteDto) {
+		Cliente clienteEntity = new Cliente();
+		clienteEntity.setNombre(clienteDto.getNombre());
+		clienteEntity.setApellido(clienteDto.getApellido());
+		clienteEntity.setEmail(clienteDto.getEmail());
+		
+		return clienteRepository.save(clienteEntity);
+	}
+	
+	//Eliminar Cliente
+	public void deleteCliente(Long id) {
+		clienteRepository.deleteById(id);
+	}
+}
